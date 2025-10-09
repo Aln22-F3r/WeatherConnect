4b54effa3233ca3773b0ed23691d396f580d32b2
Alan Ramos <alanzon132@gmail.com>
2025-10-08 21:00:28 -0600

    Ahora en la terminal te permite poner la longitud y la latitud para pedir los datos de esas coordenadas.

--- backend/WeatherAPI.java
+++ backend/WeatherAPI.java
@@ -4,11 +4,20 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Scanner;
 
 public class WeatherAPI {
     public static void main(String[] args) {
-        String urlString = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=5f1afc54ccc5cec288756eb86a9f518b";
-        
+        String apiKey = "5f1afc54ccc5cec288756eb86a9f518b";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingresa la Latitud");
+        String lat = scanner.nextLine();
+
+        System.out.println("Ingresa la Longitud");
+        String lon = scanner.nextLine();
+        String urlString = "https://api.openweathermap.org/data/2.5/weather?lat="
+                + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=metric&lang=es";
         try {
             URL url = new URL(urlString);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -31,7 +40,9 @@ public class WeatherAPI {
                 System.out.println("Error al conectar: " + status);
             }
         } catch (Exception e) {
+            System.out.println("Error: ");
             e.printStackTrace();
         }
+        scanner.close();
     }
 }
